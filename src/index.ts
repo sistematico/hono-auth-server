35a47a72de6987bf5f81f7eed3adec0971e7f9e3
Lucas Saliés Brum <sistematico@gmail.com>
2023-10-05 00:16:03 -0400

    First commit

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,32 @@
-import { Hono } from 'hono'
-import { cors } from 'hono/cors'
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { PrismaClient } from "@prisma/client";
 
-const app = new Hono()
+interface User {
+  email: string
+  password: string
+}
 
-app.use(cors())
+const prisma = new PrismaClient();
+const app = new Hono();
 
-app.get('/', (c) => c.text('Hello Hono!'))
+app.use(cors());
 
-export default app
+// const count = await prisma.user.count();
+// console.log(`There are ${count} users in the database.`);
+
+async function createUser(data: User) {
+  await prisma.user.create({ data });
+}
+
+app.post("/register", async (c) => {
+  const data = await c.req.json();
+  const auth = createUser({ email: data.email, password: data.password });
+  if (!auth) return c.json({ status: 'error', message: 'Erro na criação do usuário.' })
+  const count = await prisma.user.count() + 1;
+  return c.json({ status: 'success', message: `Usuário criado com sucesso, existem ${count} usuários no nosso banco de dados.` })
+});
+
+app.get("/", (c) => c.json({ message: "Hello Hono!" }));
+
+export default app;
